014f8bbb103b6c7ba9bf3d9ac099df89c9ce12f4
frog-slayer <pj0642@gmail.com>
2025-05-17 05:44:21 +0900

    [feat] 헤더, PillButton, Avatar 구현 #1

---
+++ src/components/layout/Header/Notification.tsx
@@ -0,0 +1,13 @@
+import { IoIosNotificationsOutline } from "react-icons/io";
+import styles from "./Notification.module.css"
+
+const Notification = () => {
+
+    return (
+        <div> 
+            <IoIosNotificationsOutline size="22px"/>
+        </div>
+    )
+}
+
+export default Notification;
