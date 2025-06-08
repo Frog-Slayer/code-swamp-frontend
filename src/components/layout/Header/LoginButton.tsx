29ee9bbe88c68d854b45ac2e98e8686b62acc763
frog-slayer <pj0642@gmail.com>
2025-06-08 20:33:39 +0900

    [feat] 새 글 쓰기 버튼 -> /post로 라우팅 #5

--- src/components/layout/Header/LoginButton.tsx
+++ src/components/layout/Header/LoginButton.tsx
@@ -1,14 +1,8 @@
 import PillButton from "@/components/ui/PillButton/PillButton";
 
-
 const LoginButton = () => {
-    const handleClick = () => {
-        alert("click login")
-    }
-
     return (
         <PillButton 
-            onClick={handleClick}
             label="로그인"
         />
     )
